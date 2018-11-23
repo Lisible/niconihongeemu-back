132da37686ec3c2776ef222fdaa2cbca53ca3b8c
Clément Sibille <clement.sibille@protonmail.com>
2018-11-23 11:13:42 +0100

    Added node-fetch module

--- routes/dictionnary.js
+++ routes/dictionnary.js
@@ -1,8 +1,12 @@
 let express = require('express');
+let fetch = require('node-fetch');
 let router = express.Router();
 
-router.get('/', function(req, res){
-
+router.get('/any/:query', function(req, res){
+    const ANY_TARGET = 'https://api.nihongoresources.com/dict/find/' + req.params.query;
+    fetch(ANY_TARGET)
+    .then(response => response.json())
+    .then(response => res.send(response));
 });
 
 module.exports = router;
