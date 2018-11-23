6ad6952780fb1f893082cc9c0252311ecdc34c21
Haize Weiss <haize.weiss@gmail.com>
2018-11-23 12:15:06 +0100

    Added find kanji route

--- routes/dictionnary.js
+++ routes/dictionnary.js
@@ -1,8 +1,11 @@
 let express = require('express');
 let router = express.Router();
 
-router.get('/', function(req, res){
-
+router.get('/kanji/:query', function(req, res){
+	const KANJI_TARGET = "https://api.nihongoresources.com/kanji/find/" + req.param.query;
+	fetch(KANJI_TAGET)
+	.then(response => response.json())
+	.then(response => console.log(response));
 });
 
 module.exports = router;
